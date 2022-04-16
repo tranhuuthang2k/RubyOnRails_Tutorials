bf655341160a5a403111d307768860cb6b74953f
tranhuuthang2k <tranhuuthangcoi@gmail.com>
2022-04-16 20:42:48 +0700

    update

--- internal social network/bt20/db/migrate/20210811090737_drop_tablename.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class DropTablename < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :products
-  end
-end
--- internal social network/bt20/db/seeds.rb
+++ internal social network/bt20/db/seeds.rb
@@ -7,9 +7,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# require 'faker'
-# 99.times do |index|
-#  name = Faker::Name.name      #=> "Christophe Bartell"
-#  email = Faker::Internet.email
-#  User.create(name: name, email:"thang_#{index}@gmail.com", password:'123', password_confirmation:'123', activated: true, activated_at: Time.zone.now)
-# end
+require 'faker'
+99.times do |index|
+ name = Faker::Name.name      #=> "Christophe Bartell"
+ email = Faker::Internet.email
+ User.create(name: name, email:"thang_#{index}@gmail.com", password:'123', password_confirmation:'123', activated: true, activated_at: Time.zone.now)
+end
